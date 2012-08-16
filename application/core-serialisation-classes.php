93224e00e59e3254a23c69ed24c69d1c094b3b00
phluid61 <matthew@kerwin.net.au>
2012-08-16 14:26:23 +1000

    added Apache License v2.0

--- application/core-serialisation-classes.php
+++ application/core-serialisation-classes.php
@@ -1,5 +1,22 @@
 <?php
 /*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  QUT licenses this file to you
+ * under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
  * NOTE: because the classes defined in this file exhibit catch-all
  *       behaviour (i.e. will attempt to handle as many requests as
  *       possible), they should be registered AFTER any more specific
--- application/representations/zz_core-serialisation.php
+++ application/representations/zz_core-serialisation.php
@@ -1,5 +1,22 @@
 <?php
 /*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  QUT licenses this file to you
+ * under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
  * Note: because the classes defined in the core-serialisation-classes
  *       file exhibit catch-all behaviour (i.e. they attempt to handle
  *       as many requests as possible) they must be registered AFTER

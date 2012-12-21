71339d56beeb68335a576bcb5e38c656094aa609
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2012-12-21 11:14:37 +1000

    added a 'touch' script to magically update Application::VERSION

--- system/application.inc.php
+++ system/application.inc.php
@@ -51,8 +51,8 @@ require_once(SYSDIR.'/utils/html.inc.php');
  * Not instantiable.
  */
 class Application {
-	/** The application framework version.  Updated by hand. */
-	const VERSION = '20121218';
+	/** The application framework version.  Updated by hand. (see: ./touch ) */
+	const VERSION = '1.0-r180';
 	/** The application framework name. */
 	const TITLE = 'REST-RMR';
 

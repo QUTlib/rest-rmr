5b8eb66c1851be51052857af47ef40e8412e0ac1
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-01-09 10:10:51 +1000

    fixed touch-script to use current (not next) commit number

--- system/application.inc.php
+++ system/application.inc.php
@@ -52,7 +52,7 @@ require_once(SYSDIR.'/utils/html.inc.php');
  */
 class Application {
 	/** The application framework version.  Updated by hand. (see: ./touch ) */
-	const VERSION = '1.0-r188';
+	const VERSION = '1.0-r189';
 	/** The application framework name. */
 	const TITLE = 'REST-RMR';
 

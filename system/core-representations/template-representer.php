9a0a4b4d10549c0763487950b21322cb1404dba7
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-02-18 17:27:29 +1000

    moved TemplateEngine representer to the same place as other core representers

---
+++ system/core-representations/template-representer.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  QUT licenses this file to you
+ * under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * This representer specialises in pushing TemplateEngine data models
+ * as 'text/html'.
+ *
+ * See `{SYSDIR}/utils/template-engine.inc.php`
+ */
+class TemplateRepresenter extends BasicRepresenter {
+
+	public function __construct() {
+		parent::__construct(
+			array(
+				new InternetMediaType('text', 'html', 1.0, TRUE),
+				new InternetMediaType('*', '*', 0.001, FALSE, 'text/html'),
+			),
+			array(),
+			array(),
+			array(
+				'object:TemplateEngine',
+			)
+		);
+	}
+
+	public function represent($m, $t, $c, $l, $response) {
+		$response->content_type("text/html; charset=utf-8");
+		// todo: magical translation magic? (fixme)
+		if ($lang = $m->language())
+			$response->content_language($lang);
+		$response->header('X-UA-Compatible', 'IE=edge');
+		$response->body( $m->execFile() );
+	}
+
+}
+
+// --- IMPORTANT: REMEMBER THIS BIT!
+
+Application::register_representer( new TemplateRepresenter() );
+
+// ----------------------------------------------------------------------
+

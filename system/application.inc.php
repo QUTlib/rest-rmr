641356f1e54fc084d27a19509df86aa6f09f3350
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-02-25 15:57:05 +1000

    moved uri-registrars back to SYSDIR

--- system/application.inc.php
+++ system/application.inc.php
@@ -24,8 +24,8 @@ add_include_path(APPDIR);
 require_once(SYSDIR.'/http-exception.inc.php');
 
 require_once(SYSDIR.'/uri-map.inc.php');
-require_once(SYSDIR.'/resource-reg/uri-registrar.inc.php');
-require_once(SYSDIR.'/resource-reg/interfaced-uri-registrar.inc.php');
+require_once(SYSDIR.'/uri-registrar.inc.php');
+require_once(SYSDIR.'/interfaced-uri-registrar.inc.php');
 
 require_once(SYSDIR.'/representation-manager.inc.php');
 require_once(SYSDIR.'/representer.inc.php');
@@ -52,7 +52,7 @@ Autoloader::register('TemplateEngine', SYSDIR.'/utils/template-engine.inc.php');
  */
 class Application {
 	/** The application framework version.  Updated by hand. (see: ./touch ) */
-	const VERSION = '1.0-r230';
+	const VERSION = '1.0-r231';
 	/** The application framework name. */
 	const TITLE = 'REST-RMR';
 
---
+++ system/interfaced-uri-registrar.inc.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * See the NOTICE file distributed with this work for information
+ * regarding copyright ownership.  QUT licenses this file to you
+ * under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('uri-registrar.inc.php');
+
+class InterfacedURIRegistrar extends URIRegistrar {
+	private $module = null;
+	private $interface = null;
+
+	/**
+	 * Constructs a new registrar, on which one may call #register_handler
+	 */
+	public function __construct($module, $interface=NULL) {
+		$this->module = $module;
+		if (func_num_args() > 1)
+			$this->set_interface($interface);
+	}
+
+	/**
+	 * Update the current interface of this registrar.
+	 *
+	 * @see the IF_... consts in Application
+	 */
+	public function set_interface($interface) {
+		$this->interface = $interface;
+		$this->prefix = '/' . $this->interface . '/' . $this->module;
+		if (!preg_match('#^/[a-z]+/[a-z_][a-z0-9_-]*$#', $this->prefix))
+			throw new Exception("invalid interface/module ['{$this->prefix}']");
+	}
+
+	/**
+	 * Sets up a handler to accept incoming requests.
+	 *
+	 * Note that GET handlers automatically set up an identical HEAD handler.
+	 *
+	 * URI pattern examples:
+	 *   '/students/:sid/'
+	 *     :: '/students/123/' => {"sid":"123"}
+	 *   '/some/path/?'
+	 *     :: '/some/path/' => {}
+	 *     :: '/some/path'  => {}
+	 *   '/branch/:name/?'
+	 *     :: '/branch/gp/' => {"name":"gp"}
+	 *     :: '/branch/kg'  => {"name":"kg"}
+	 *     :: '/branch/'    => FALSE
+	 *
+	 * @param String $http_method the HTTP method to handle (e.g. GET, POST, etc.).
+	 * @param String $uri_pattern
+	 * @param Mixed $handler 'function', 'class->method', 'class::static_method', array(object,'method'), array('class','method')
+	 */
+	public function register_handler($http_method, $uri_pattern, $handler) {
+		if (is_null($this->interface)) throw new Exception("interface not set");
+		parent::register_handler($http_method, $uri_pattern, $handler);
+	}
+
+}
+

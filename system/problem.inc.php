ba1e0c54fbb1acc13bb7729d24bfbf02a2ae7be7
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-05-29 14:56:12 +1000

    got rid of some java-ased silliness, added some docs

--- system/problem.inc.php
+++ system/problem.inc.php
@@ -33,34 +33,43 @@ class Problem {
 	/**
 	 * Creates a new Problem, with the minimum set of required attributes.
 	 */
-	public function __construct($problemType, $title, $httpStatus) {
+	public function __construct($problemType, $title, $httpStatus, $detail=null) {
 		$this->problemType = "$problemType";
 		$this->title = "$title";
 		$this->httpStatus = (int)$httpStatus;
+		if (func_num_args() > 3) {
+			$this->detail = "$detail";
+		}
 	}
 
-	public function problemType() { return "{$this->problemType}"; }
-	public function title() { return "{$this->title}"; }
+	/** Get the immutable problemType URI of this problem. */
+	public function problemType() { return $this->problemType; }
+	/** Get the immutable short, human-readable summary of this problem. */
+	public function title() { return $this->title; }
+	/** Get the immutable HTTP status code set by the server for this occurrence of the problem. */
 	public function httpStatus() { return $this->httpStatus; }
 
+	/** Get or set the optional human readable explanation specific to this occurrence of the problem. */
 	public function detail($val=null) {
 		if (func_num_args() == 0) return $this->detail;
 		$this->detail = "$val";
 	}
 
+	/** Get or set the optional URI that identifies the specific occurrence of the problem. */
 	public function problemInstance($val=null) {
 		if (func_num_args() == 0) return $this->problemInstance;
 		$this->problemInstance = "$val";
 	}
 
+	/** Get an associative-array representation of this problem object. */
 	public function to_array() {
 		$array = array(
-			'problemType' => "{$this->problemType}",
-			'title' => "{$this->title}",
+			'problemType' => $this->problemType,
+			'title' => $this->title,
 			'httpStatus' => $this->httpStatus,
 		);
-		if (isset($this->detail)) $array['detail'] = "{$this->detail}";
-		if (isset($this->problemInstance)) $array['problemInstance'] = "{$this->problemInstance}";
+		if (isset($this->detail)) $array['detail'] = $this->detail;
+		if (isset($this->problemInstance)) $array['problemInstance'] = $this->problemInstance;
 		return $array;
 	}
 }

f305127a5188d1661a5f4f79e1c1e3756bc38326
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-07-02 13:59:44 +1000

    version bump

--- system/application.inc.php
+++ system/application.inc.php
@@ -55,7 +55,7 @@ Autoloader::register('TemplateEngine', SYSDIR.'/utils/template-engine.inc.php');
  */
 class Application {
 	/** The application framework version.  Updated by hand. (see: ./touch ) */
-	const VERSION = '1.0-r317';
+	const VERSION = '1.0-r320';
 	/** The application framework name. */
 	const TITLE = 'REST-RMR';
 

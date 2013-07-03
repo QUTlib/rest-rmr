50f3108da66a1913db992c4c8028877af326a331
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2013-07-03 14:43:43 +1000

    incremented version to 1.1

--- system/application.inc.php
+++ system/application.inc.php
@@ -55,7 +55,7 @@ Autoloader::register('TemplateEngine', SYSDIR.'/utils/template-engine.inc.php');
  */
 class Application {
 	/** The application framework version.  Updated by hand. (see: ./touch ) */
-	const VERSION = '1.0-r330';
+	const VERSION = '1.0-r335';
 	/** The application framework name. */
 	const TITLE = 'REST-RMR';
 
